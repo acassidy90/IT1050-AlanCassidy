7c484a391706a9d169a809bb184f1fdb8e5d9e66
acassidy90 <31634606+acassidy90@users.noreply.github.com>
2017-09-04 19:00:27 -0500

    Set up Command Prompt

---
+++ Labzero1/Labzero1/lab01.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labzero1
+{
+    class lab01
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! From Alan Cassidy");
+        }
+    }
+}
