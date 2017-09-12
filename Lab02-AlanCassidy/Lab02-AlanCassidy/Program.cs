23209d33ea1b56969092083c5f1e7ecbb4205d83
acassidy90 <31634606+acassidy90@users.noreply.github.com>
2017-09-11 23:01:33 -0500

    Hello World Tab

--- Lab02-AlanCassidy/Lab02-AlanCassidy/Program.cs
+++ Lab02-AlanCassidy/Lab02-AlanCassidy/Program.cs
@@ -10,23 +10,7 @@ namespace Lab02_AlanCassidy
     {
         static void Main(string[] args)
         {
-            int x; // stores first number to be entered by user
-            int y; // stores second number to be entered by user
-            int z; // stores third number to be entered by user
-            int result; // product of numbers
-
-            Console.Write("enter first integer:"); // prompt for input
-            x = Convert.ToInt32(Console.ReadLine()); // read first integer
-
-            Console.Write("enter second integer:"); // prompt for input
-            y = Convert.ToInt32(Console.ReadLine()); // read second 
-
-            Console.Write("enter third integer:"); // prompt for input
-            z = Convert.ToInt32(Console.ReadLine()); // read third integer
-
-            result = (x + y) * (z + 10); // calculate product of numbers
-
-            Console.WriteLine("Product is {0}", result);
+            Console.WriteLine("Hello\tWorld");
         } // end main
     } // end class addition
 }
